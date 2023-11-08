ccb9445db913e3829c99e605fa787c2cc8264b47
Jonathan Clark <jonathan@hexa3d.io>
2023-11-08 13:17:02 +0200

    Remove the whole loop

--- mvdiffusion/pipelines/pipeline_mvdiffusion_image.py
+++ mvdiffusion/pipelines/pipeline_mvdiffusion_image.py
@@ -433,43 +433,25 @@ class MVDiffusionImagePipeline(DiffusionPipeline):
         # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
         extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
 
-        # 7. Denoising loop
-        num_warmup_steps = len(timesteps) - num_inference_steps * self.scheduler.order
-        with self.progress_bar(total=num_inference_steps) as progress_bar:
-            for i, t in enumerate(timesteps):
-                # expand the latents if we are doing classifier free guidance
-                # latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
-                # latent_model_input = torch.cat([
-                #     latent_model_input, image_latents
-                # ], dim=1)
-                # latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
-                noise = torch.randn_like(latents)
+        # 7. Denoising
+        t = torch.randint(0, 1, (latents.shape[0],), dtype=torch.long, device=self.device)
 
-                latents_noisy = self.scheduler.add_noise(latents, noise, t)
-                latent_model_input = torch.cat([latents_noisy] * 3)
-                tt = torch.cat([t] * 3)
+        noise = torch.randn_like(latents)
+        latents_noisy = self.scheduler.add_noise(latents, noise, t)
+        latent_model_input = torch.cat([latents_noisy] * 3)
+        tt = torch.cat([t] * 3)
+        noise_pred = self.unet(latent_model_input, tt, encoder_hidden_states=image_embeddings).sample
+        noise_pred_uncond, noise_pred_y, noise_pred_neg = noise_pred.chunk(3)
+        noise_pred = (noise_pred_y - noise_pred_uncond) * guidance_scale  # delta_c
 
-                # Use noise free score distillation from https://github.com/orenkatzir/nfsd
-                noise_pred = self.unet(latent_model_input, tt, encoder_hidden_states=image_embeddings, class_labels=camera_embeddings).sample
+        if t < 200:  # delta_d
+            # epsilon(zt,none,t)
+            noise_pred += noise_pred_uncond
+        else:
+            # epsilon(zt,none,t)-epsilon(zt,neg,t)
+            noise_pred += (noise_pred_uncond - noise_pred_neg)
 
-                # perform guidance
-                if do_classifier_free_guidance:
-                    noise_pred_uncond, noise_pred_y, noise_pred_neg = noise_pred.chunk(3)
-                    noise_pred = (noise_pred_y - noise_pred_uncond) * guidance_scale  # delta_c
-
-                    if t < 200:  # delta_d
-                        noise_pred += noise_pred_uncond
-                    else:
-                        noise_pred += (noise_pred_uncond - noise_pred_neg)
-
-                # compute the previous noisy sample x_t -> x_t-1
-                latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
-
-                # call the callback, if provided
-                if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
-                    progress_bar.update()
-                    if callback is not None and i % callback_steps == 0:
-                        callback(i, t, latents)
+        latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
 
         if not output_type == "latent":
             if num_channels_latents == 8:
